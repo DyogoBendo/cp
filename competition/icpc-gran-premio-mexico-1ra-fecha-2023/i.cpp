df2c28b0e61e670ebfb7cb8a0fae4978167526f0
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2025-03-12 18:45:38 -0300

    add

--- competition/icpc-gran-premio-mexico-1ra-fecha-2023/i.cpp
+++ competition/icpc-gran-premio-mexico-1ra-fecha-2023/i.cpp
@@ -10,38 +10,35 @@ pair<int, string> check1(vector<pair<int, int>> blocks){
     int l = 0, r = 0, ans = 0, total = 0;
     int st = -1, en = -1;
     while(r < n){
-        oc[blocks[r].second] += blocks[r].first;
-        int curr = oc[blocks[r].second];
-        while(curr > k){
+        oc[blocks[r].second] += blocks[r].first;                 
+        total += blocks[r].first;       
+        while(oc[blocks[r].second] > k){
             total -= blocks[l].first;
-            oc[blocks[l].second] -= blocks[l].first;
-
-            if(blocks[l].second == blocks[r].second) curr -= blocks[l].first;
+            oc[blocks[l].second] -= blocks[l].first;            
             l++;
-        }
+        }                
         if(total > ans){
             ans = total;
             st = l;
             en = r;
         }        
         r ++;        
-    }
+    }    
 
     string s = "";
     for (int i = st; i <= en; i++)
     {
+        char c = blocks[i].second + 'a';        
         for (int j = blocks[i].first; j; j--)
         {
-            char c = blocks[i].second + 'a';
             s += c;
-        }
-        
-    }
+        }        
+    }    
     
     return {ans, s};
 }
 
-tuple<int, string> check2(vector<pair<int, int>> blocks){
+pair<int, string> check2(vector<pair<int, int>> blocks){
     vector<int> oc(26, 0);
     int ans = 0;
     int n = blocks.size();
@@ -52,26 +49,42 @@ tuple<int, string> check2(vector<pair<int, int>> blocks){
         oc[blocks[prefix_end].second] += blocks[prefix_end].first;
         int curr = oc[blocks[prefix_end].second];
         total += blocks[prefix_end].first;
-        if(curr > k) break;
+        if(curr > k){
+            total -= blocks[prefix_end].first;
+            break;
+        }
         ans = total;
 
         prefix_end++;
-    }
-    if(prefix_end == n) return {ans, 0, prefix_end - 1};
+    }    
+    if(prefix_end == n){
+        string s = "";
+        for (auto b : blocks)
+        {
+            for (int j = 0; j < b.first; j++)
+            {
+                s += 'a' + b.second;
+            }            
+        }        
+        return {ans, s};        
+    } 
 
+    int prefix_ans = prefix_end;
     oc[blocks[prefix_end].second] -= blocks[prefix_end].first;
     prefix_end --;
     int suffix_begin = n - 1;    
     int st = -1, en = -1;
-    while(suffix_begin >= 0){
+
+    while(suffix_begin >= 0){                
         oc[blocks[suffix_begin].second] += blocks[suffix_begin].first;                
+        total += blocks[suffix_begin].first;
         while(oc[blocks[suffix_begin].second] > k && prefix_end >= 0){
             oc[blocks[prefix_end].second] -= blocks[prefix_end].first;
             total -= blocks[prefix_end].first;
             prefix_end --;
         }        
         if(oc[blocks[suffix_begin].second] > k) break;
-        if(total > ans){
+        if(total > ans){            
             ans = total;
             st= prefix_end;
             en = suffix_begin;
@@ -80,11 +93,27 @@ tuple<int, string> check2(vector<pair<int, int>> blocks){
         
         suffix_begin--;
     }
-
+    
+    if(st == -1){
+        st = prefix_ans;
+        en = n;
+    }    
     string s = "";
-    for
-
-    return ans + 1;
+    for (int i = en; i < n; i++)
+    {
+        for (int j = 0; j < blocks[i].first; j++)
+        {
+            s += 'a' + blocks[i].second;
+        }            
+    }        
+    for (int i = 0; i <= st; i++)
+    {
+        for (int j = 0; j < blocks[i].first; j++)
+        {
+            s += 'a' + blocks[i].second;
+        }            
+    }        
+    return {ans, s};
 }
 
 void solve(){
@@ -111,16 +140,23 @@ void solve(){
         s.push_back(s[i]);
     }
     
-    s = s.substr(i);
+    s = s.substr(i);    
+    
     vector<pair<int, int>> blocks;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n;)
     {        
         int j;
-        for(j = i; j < n && s[j] == s[i]; j++);
+        for(j = i; j < n && s[j] == s[i]; j++);        
         blocks.push_back({j - i, s[i] - 'a'});
+        i = j;
+    }    
+    auto ans1 = check1(blocks);
+    auto ans2 = check2(blocks);    
+
+    if(ans2.first > ans1.first){
+        swap(ans1, ans2);
     }
-    
-    cout << max(check1(blocks), check2(blocks)) << endl;
+    cout << ans1.first << endl << ans1.second << endl;    
 }
 
 signed main(){
