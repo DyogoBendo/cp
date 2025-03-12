41f0cfaaa9b3a1ad17224be614442d298f93aeeb
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2025-03-12 18:59:49 -0300

    add

--- rated-questions/1300/1178B.cpp
+++ rated-questions/1300/1178B.cpp
@@ -3,33 +3,38 @@ using namespace std;
 
 #define darvem ios_base::sync_with_stdio(false);cin.tie(NULL)
 #define endl '\n'
-
+#define ll long long
 
 void solve(){
     string s;
-    cin >> s;
-
-    int curr = 0;
-    int ans = 0;
-
-    int o_cnt = 0;
+    cin >> s;    
+    ll ans = 0;
 
     int n = s.size();
-    
+
+    vector<int> psum(n + 1, 0);
+
     for (int i = 0; i < n; i++)
     {
-        int cnt= 0;
-        while(i < n && s[i] == 'v'){
-            cnt ++;
+        if(i > 0 && s[i] != 'v' && s[i - 1] == 'v') psum[i] --;
+        psum[i + 1] += psum[i] + (s[i] == 'v'); 
+    }    
+    if(s[n - 1] == 'v') psum[n]--;
+
+    for (int i = 0; i < n; i++)
+    {
+        if(s[i] == 'o'){            
+            ans += (ll) (psum[i]) * (psum[n] - psum[i + 1]);
         }
     }
+    
+    cout << ans << endl;
     
 }
 
 signed main(){
     darvem;
-    int t = 1;
-    cin >> t;
+    int t = 1;    
 
     while(t--) solve();
 }
