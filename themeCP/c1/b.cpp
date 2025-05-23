17007665dcddde59d6add7919798536aaf38fa32
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2025-05-23 06:25:34 -0300

    add

--- themeCP/c1/b.cpp
+++ themeCP/c1/b.cpp
@@ -9,14 +9,37 @@ void solve(){
     int n;
     cin >> n;
 
-    if(n >= 3) cout << 3 << " ";
-    cout << 1 << " ";
+    vector<int>v(n);
+
+    if(n == 1){
+        cout << 1 << endl;
+        return;
+    }
+    if(n == 2){
+        cout << 1 << " " << 2 << endl;
+        return;
+    }
+    v[0] = 3;
+    v[n-1] = 2;
+    v[n/2] = 1;
+    int idx = 1;
     for (int i = 4; i <= n; i++)
     {
-        cout << i << " ";
+        if(!v[idx]){
+            v[idx] = i;
+        } else{
+            idx++;
+            v[idx] = i;
+        }
+        idx++;
     }
-    if(n >= 2) cout << 2;
+
+    for (auto a : v)
+    {
+        cout << a << " ";
+    }
     cout << endl;
+    
 }
 
 
