0c0775b6fd32fa56ca938a65213e15d98643cdad
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2025-06-24 07:23:17 -0300

    add

--- competition/icpc/latam/brasil/2023/subregional/h.cpp
+++ competition/icpc/latam/brasil/2023/subregional/h.cpp
@@ -1,90 +1,53 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
-#define all(a) a.begin(), a.end()
-#define rall(a) a.rbegin(), a.rend()
-#define sz(a) (int) (a).size()
-#define ll long long
-#define ld long double
-#define pb push_back
+    #define darvem ios_base::sync_with_stdio(false);cin.tie(NULL)
+    #define endl '\n'
+    #define ll long long
+    #define int long long
 
-typedef long long C;
-typedef complex<C> P;
-#define X real()
-#define Y imag()
 
-void dbg_out() { cerr << endl; }
-template <typename H, typename... T>
-void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
-#define dbg(...) { cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__); }
+    signed main(){
+        darvem;
+        int n, s;
+        cin >> n >> s;        
 
-void solution(){
-    ll n, val;
-    cin >> n >> val;
-    multiset<ll> cur;
-    vector<tuple<ll, ll, ll>> arr;
-    for (int i=0; i<n; i++){
-        int a, b, c;
-        cin >> a >> b >> c;
-        arr.push_back({a, b, c});
-    }
-    sort(all(arr));
-    ll ans=0;
-    priority_queue<pair<ll, ll>> to_remove, to_add;
-    for (auto [a, b, c]: arr){
+        vector<tuple<int, int, int>> v;  
+        vector<tuple<int, int, int>> original;
+        for (int i=1; i<=n; i++){
+            int a, b, c;
+            cin >> a >> b >> c;
+            
+            v.push_back({a, -i, c});
+            v.push_back({b, i, c});
+            original.push_back({a, b, c});
+        }    
 
-        while (!to_add.empty()){
-            ll x, v;
-            tie(x, v) = to_add.top();
-            x = -x;
-            if (x > a) break;
-            cur.insert(v);
-            to_add.pop();
+        sort(v.begin(), v.end());
+
+        set<pair<int, int>> open;
+        vector<int> st(n+1);
+        int ans = 0;
+
+        for (auto [pos, idx, c] : v)
+        {        
+            if(idx < 0){
+                idx = -idx;
+                int bst = ans;
+                if(open.size()){
+                    bst =max(ans, open.rbegin()->first + (pos - 1) * s);
+                }
+                int base = bst - c - (pos - 1) * s;
+                st[idx] = base;
+                open.insert({base, idx});
+                st[idx];
+            } else{            
+                int bst = open.rbegin()->first + (pos) * s;
+                ans = max(ans, bst);
+
+                open.erase({st[idx], idx});
+            }   
         }
 
-        while (!to_remove.empty()){
-            ll x, v;
-            tie(x, v) = to_remove.top();
-            x = -x;
-            if (x >= a) break;
-            ans = max(ans, v+(x+1)*val);
-            cur.erase(cur.find(v));
-            to_remove.pop();
-        }
-        
-        ll prev=0;
-        if (!cur.empty()) prev = max(prev, *cur.rbegin()+a*val);
-
-        to_add.push({-a, max(prev, ans)-a*val-c});
-        to_remove.push({-b, max(prev, ans)-a*val-c});
+        cout << ans << endl;
     }
-
-    while (!to_add.empty()){
-        ll x, v;
-        tie(x, v) = to_add.top();
-        x = -x;
-        cur.insert(v);
-        to_add.pop();
-    }
-
-    while (!to_remove.empty()){
-        ll x, v;
-        tie(x, v) = to_remove.top();
-        x = -x;
-        ans = max(ans, v+(x+1)*val);
-        cur.erase(cur.find(v));
-        to_remove.pop();
-    }
-
-    cout << ans << '\n';
-}
-
-signed main(){
-    fastio;
-    int n=1;
-    //cin >> n;
-    for (int i=0; i<n; i++){
-        solution();
-    }
-}
