b7e79e62350193ded7e98cde2256f262d92f2f74
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2025-06-30 22:07:07 -0300

    add

--- subject/geometry/line_segment_intersection.cpp
+++ subject/geometry/line_segment_intersection.cpp
@@ -34,6 +34,12 @@ ll ccw(pt p1, pt p2, pt p3){
     return v1 ^ v2;
 }
 
+ll dst2(pt p1, pt p2){
+    ll dx = p1.x - p2.x;
+    ll dy = p1.y - p2.y;
+    return  dx*dx+ dy*dy;
+}
+
 void solve(){
     vector<pt> v(4);
 
@@ -48,13 +54,22 @@ void solve(){
     ll a4 = ccw(v[2], v[1], v[3]);
 
     if(a1 == a2 && a1 == a3 && a3 == a4 && a1 == 0){ // pertencem a mesma reta
-        int st1 = min(v[0].x, v[1].x);
-        int en1 = max(v[0].x, v[1].x);
-        int st2 = min(v[2].x, v[3].x);
-        int en2 = max(v[2].x, v[3].x);
-        if((st1 <= v[2].x && v[2].x <= en1) or (st1 <= v[3].x && v[3].x <= en1) or (st2 <= v[0].x && v[0].x <= en2) or (st2 <= v[1].x && v[1].x <= en2)) cout << "YES" << endl;
-        else cout << "NO" << endl;
-    } else{
+
+        if(v[1] < v[0]) swap(v[0], v[1]);
+        if(v[3] < v[2]) swap(v[2], v[3]);
+
+        ll d1 = dst2(v[0], v[1]), d2 = dst2(v[0] , v[2]), d3 = dst2(v[2] , v[3]);  
+        
+        if(v[0] < v[2]){
+            if( d1>=d2 ) cout << "YES" << endl;
+            else cout << "NO" << endl;
+        } else{
+            if(d3>= d2) cout << "YES" << endl;
+            else cout << "NO" << endl;
+        }
+        
+        
+    } else{     
         if(((a1 <= 0 && a2 >= 0) || (a1 >= 0 && a2 <= 0)) && ((a3 <= 0 && a4 >=0) || (a3 >=0 && a4 <= 0))) cout << "YES" << endl;
         else cout << "NO" << endl;
     }
