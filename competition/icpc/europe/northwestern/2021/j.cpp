31ae12696ea1ccf322274fff3361720e3e3820c6
Dyogo Romagna Bendo <dyogo.bendo@pti.org.br>
2025-07-04 09:43:55 -0300

    add

--- competition/icpc/europe/northwestern/2021/j.cpp
+++ competition/icpc/europe/northwestern/2021/j.cpp
@@ -1,62 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio cin.tie(0)->sync_with_stdio(0)
+#define darvem ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define endl '\n'
 #define ll long long
-#define all(a) a.begin(), a.end()
-#define rall(a) a.rbegin(), a.rend()
-#define sz(a) ((int)(a).size())
-
-vector<int> visited (1000);
-
-void solve(){
-    int n=1;
-    cin >> n;
-    vector<int> arr (n);    
-    int trash;
-    for (auto &a: arr) cin >> trash >> a;
-    arr.push_back(arr[0]);
-
-    int last=(arr[0] + 180);
-    bool found = false;
-
-    for (int i=1; i<n+1; i++){
-        int lon = (arr[i] + 180);        
-        
-        int mn = 2*min(last, lon), mx = 2*max(last, lon);
-        
-        int d = 360 + (mn - mx);                
-
-        if(d < 0){
-            for (int j = 0; j <= mn; j++)visited[j] = 1;
-            for (int j = mx; j < 720; j++)visited[j] = 1;            
-        }else if(d > 0)for(int j = mn; j<= mx; j++) visited[j] = 1;
-        else found = true;        
-                
-        last = lon;
-    }
-    int cant=-1;
-    
-    for (int i=0; i<720; i++){
-        if (!visited[i]){
-            cant = i;            
-        } 
-    } 
-        
-    if (cant == -1 or found){        
-        cout << "yes";
-    } 
-    else{            
-        double ans = cant / 2.0 - 180;        
-        cout << setprecision(1) << fixed;
-        cout << "no " << ans;
-    } 
-    cout << '\n';
-}
 
 signed main(){
-    fastio;
-    int t=1;
-    //cin >> t;
-    for (int i=0; i<t; i++) solve();
+    darvem;
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x >> v[i];
+        v[i] += 180;
+        v[i] *= 2;
+    }
+
+    vector<int> visited(720);
+
+    for (int i = 1; i <= n; i++)
+    {
+        int prev = v[i-1], curr = v[i%n];
+        if(prev > curr) swap(prev, curr);
+
+        int d1 = prev + (720 - curr);
+        int d2 = curr - prev;
+
+        if(d1 < d2){
+            for(int i = 0; i <= prev; i++) visited[i] = 1;
+            for(int i = curr; i < 720; i++) visited[i] = 1;
+        } else if(d2 < d1){
+            for(int i = prev; i <= curr; i++) visited[i] = 1;
+        } else{
+            cout << "yes" << endl;
+            return 0;
+        }
+    }
+    
+    for(int i = 0; i < 720; i++){
+        if(!visited[i]){
+            double ans = i/2.0 - 180;
+            cout << setprecision(1) << fixed;
+            cout << "no " << ans << endl;
+            return 0;
+        }
+    }
+    
+    cout << "yes" << endl;
 }
