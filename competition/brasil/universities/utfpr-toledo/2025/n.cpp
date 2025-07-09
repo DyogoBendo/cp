fa30eab631f573223a40b62722f5f383345fcaeb
Dyogo Romagna Bendo <dyogo.bendo@pti.org.br>
2025-07-09 13:51:10 -0300

    add

--- competition/brasil/universities/utfpr-toledo/2025/n.cpp
+++ competition/brasil/universities/utfpr-toledo/2025/n.cpp
@@ -4,25 +4,98 @@ using namespace std;
 #define darvem ios_base::sync_with_stdio(false);cin.tie(NULL)
 #define endl '\n'
 #define ll long long
+#define int long long
 const ll MOD = 998244353;
 
-ll sum(ll x){
-    return (x)* (x + 1) / 2 % MOD;
+// Aritmetica Modular
+//
+// O mod tem q ser primo
+
+template<int p> struct mod_int {
+	ll expo(ll b, ll e) {
+		ll ret = 1;
+		while (e) {
+			if (e % 2) ret = ret * b % p;
+			e /= 2, b = b * b % p;
+		}
+		return ret;
+	}
+	ll inv(ll b) { return expo(b, p-2); }
+
+	using m = mod_int;
+	int v;
+	mod_int() : v(0) {}
+	mod_int(ll v_) {
+		if (v_ >= p or v_ <= -p) v_ %= p;
+		if (v_ < 0) v_ += p;
+		v = v_;
+	}
+	m& operator +=(const m& a) {
+		v += a.v;
+		if (v >= p) v -= p;
+		return *this;
+	}
+	m& operator -=(const m& a) {
+		v -= a.v;
+		if (v < 0) v += p;
+		return *this;
+	}
+	m& operator *=(const m& a) {
+		v =  1LL * v * (a.v) % p;
+		return *this;
+	}
+	m& operator /=(const m& a) {
+		v = v * inv(a.v) % p;
+		return *this;
+	}
+	m operator -(){ return m(-v); }
+	m& operator ^=(ll e) {
+		if (e < 0) {
+			v = inv(v);
+			e = -e;
+		}
+		v = expo(v, e);
+		// possivel otimizacao:
+		// cuidado com 0^0
+		// v = expo(v, e%(p-1)); 
+		return *this;
+	}
+	bool operator ==(const m& a) { return v == a.v; }
+	bool operator !=(const m& a) { return v != a.v; }
+
+	friend istream& operator >>(istream& in, m& a) {
+		ll val; in >> val;
+		a = m(val);
+		return in;
+	}
+	friend ostream& operator <<(ostream& out, m a) {
+		return out << a.v;
+	}
+	friend m operator +(m a, m b) { return a += b; }
+	friend m operator -(m a, m b) { return a -= b; }
+	friend m operator *(m a, m b) { return a *= b; }
+	friend m operator /(m a, m b) { return a /= b; }
+	friend m operator ^(m a, ll e) { return a ^= e; }
+};
+
+typedef mod_int<(int)MOD> mint;
+
+mint sum(mint x){
+    return (x)* (x + 1) / 2;
 }
 
-ll sum2(ll x){
-    return ((x) * (x + 1) * (2*x + 1)) / 6 % MOD;
+mint sum2(mint x){
+    return ((x) * (x + 1) * (2*x + 1)) / 6;
 }
 
 signed main(){
     darvem;
 
-    ll a, b;
-    cin >> a >> b;
-    if(a > b) swap(a, b);
-
-    ll tot = sum(a) * sum(b);
-    ll quad = a * a * b - a*(sum(a)) - b * (sum(b)) + sum2(a);
-
+    ll x, y;
+    cin >> x >> y;
+    if(x > y) swap(x, y);
+    mint a(x), b(y);
+    mint tot = sum(a) * sum(b);
+    mint quad = (a * a * b) -( (a + b) * (sum(a-1)) )+ sum2(a - 1);
     cout << tot - quad << endl;
 }
