23951c0e559379812c6f2937daf46e18fc58f23c
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2025-08-01 19:11:12 -0300

    add

---
+++ subject/graph/toposort/course_schedule_ii.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define darvem ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define endl '\n'
+#define ll long long
+
+signed main(){
+    darvem;
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> out(n+1);
+    vector<vector<int>> gi(n+1);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        gi[b].push_back(a);
+        out[a]++;
+    }
+
+    priority_queue<int> pq;
+    for(int i = 1; i<= n; i++) if(out[i] == 0) pq.push(i);
+
+    vector<int> ans;
+
+    while(!pq.empty()){
+        int curr = pq.top();
+        ans.push_back(curr); pq.pop();
+
+        for (auto e : gi[curr])
+        {
+            out[e]--;
+            if(out[e] == 0) pq.push(e);
+        }        
+    }
+
+    reverse(ans.begin(), ans.end());
+    for(auto a : ans) cout << a << " ";
+    cout << endl;
+    
+}
