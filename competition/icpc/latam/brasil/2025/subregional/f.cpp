3aaac8003ab6485436b25603991998bdfa516779
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2025-09-17 06:51:37 -0300

    add

---
+++ competition/icpc/latam/brasil/2025/subregional/f.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define darvem ios_base::sync_with_stdio(0); cin.tie(0)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(a) (int) (a).size()
+#define ll long long
+#define ld long double
+
+void dbg_out(string s) { cerr << endl; }
+template<typename H, typename... T>
+void dbg_out(string s, H h, T... t){
+    do{ cerr << s[0]; s = s.substr(1);
+    } while (sz(s) && s[0] != ',');
+    cerr << " = " << h;
+    dbg_out(s, t...);
+}
+
+#ifdef DEBUG
+#define dbg(...) dbg_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+// Aritmetica Modular
+//
+// O mod tem q ser primo
+
+template<int p> struct mod_int {
+	ll expo(ll b, ll e) {
+		ll ret = 1;
+		while (e) {
+			if (e % 2) ret = ret * b % p;
+			e /= 2, b = b * b % p;
+		}
+		return ret;
+	}
+	ll inv(ll b) { return expo(b, p-2); }
+
+	using m = mod_int;
+	int v;
+	mod_int() : v(0) {}
+	mod_int(ll v_) {
+		if (v_ >= p or v_ <= -p) v_ %= p;
+		if (v_ < 0) v_ += p;
+		v = v_;
+	}
+	m& operator +=(const m& a) {
+		v += a.v;
+		if (v >= p) v -= p;
+		return *this;
+	}
+	m& operator -=(const m& a) {
+		v -= a.v;
+		if (v < 0) v += p;
+		return *this;
+	}
+	m& operator *=(const m& a) {
+		v = 1LL * v * (a.v) % p;
+		return *this;
+	}
+	m& operator /=(const m& a) {
+		v = v * inv(a.v) % p;
+		return *this;
+	}
+	m operator -(){ return m(-v); }
+	m& operator ^=(ll e) {
+		if (e < 0) {
+			v = inv(v);
+			e = -e;
+		}
+		v = expo(v, e);
+		// possivel otimizacao:
+		// cuidado com 0^0
+		// v = expo(v, e%(p-1)); 
+		return *this;
+	}
+	bool operator ==(const m& a) { return v == a.v; }
+	bool operator !=(const m& a) { return v != a.v; }
+
+	friend istream& operator >>(istream& in, m& a) {
+		ll val; in >> val;
+		a = m(val);
+		return in;
+	}
+	friend ostream& operator <<(ostream& out, m a) {
+		return out << a.v;
+	}
+	friend m operator +(m a, m b) { return a += b; }
+	friend m operator -(m a, m b) { return a -= b; }
+	friend m operator *(m a, m b) { return a *= b; }
+	friend m operator /(m a, m b) { return a /= b; }
+	friend m operator ^(m a, ll e) { return a ^= e; }
+};
+
+typedef mod_int<(int)1e9+7> mint;
+
+signed main(){
+    darvem;
+
+    int n, q;
+    cin >> n >> q;
+
+    vector<mint> p(q);
+    p[0] = 1;
+    for(int i = 1; i < q; i++) p[i] = p[i-1] * 2;
+    vector<mint> ans(n + 1);
+
+    vector<int> queries(q);
+
+    for(int i = 0; i < q; i++) cin >> queries[i];
+
+    vector<mint> suffix(q + 1);
+    for(int i = q - 1; i>= 0; i--){
+        suffix[i] = suffix[i + 1] / 2 + queries[i];
+    }
+
+    for(int i = 0; i < q; i++){
+        int x = queries[i];
+        dbg(x, suffix[i+1]);
+        ans[x] += suffix[i + 1];
+    }
+
+    for(int i = 0; i < n; i++) cout << ans[i+1] << endl;
+}
