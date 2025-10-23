54687d582900bb9564da83dc4f099a78c080042c
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2025-10-23 09:07:26 -0300

    add

--- competition/icpc/europe/benelux/2024/m.cpp
+++ competition/icpc/europe/benelux/2024/m.cpp
@@ -53,18 +53,25 @@ signed main(){
     vector<tuple<int, int, int>> entries;    
     vector<int> a(m);
     vector<int> b(m);
-    map<int, pair<int, int>> repeated;
+    map<int, pair<int, int>> repeateds, repeatede;
     vector<int> removed(m);
     for(int i = 0; i < m; i++){
         int s, e;
         cin >> s >> e;
         
-        if(repeated[s].first){
-            if(repeated[s].first <= e){                
-                removed[repeated[s].second] = 1;                
-                repeated[s] = {e, i};
+        if(repeateds[s].first){
+            if(repeateds[s].first > e){                
+                removed[repeateds[s].second] = 1;                
+                repeateds[s] = {e, i};
             } else continue;            
-        } else repeated[s] = {e, i};
+        } else repeateds[s] = {e, i};
+
+        if(repeatede[e].first){
+            if(repeatede[e].first < s){                
+                removed[repeatede[e].second] = 1;                
+                repeatede[e] = {s, i};
+            } else continue;            
+        } else repeatede[e] = {s, i};
 
         s--; e--;        
         entries.push_back({s, 0, i});
@@ -82,7 +89,7 @@ signed main(){
         if(op == 0) curr.push_back(idx);
         else{                     
             bool found = false;
-            for(; pos < sz(curr) && !found ; pos++){
+            for(; pos < sz(curr) && !found ; pos++){                
                 if(curr[pos] != idx) removed[curr[pos]] = true;
                 if(curr[pos] == idx) found= true;
             }
@@ -91,19 +98,21 @@ signed main(){
 
     curr.clear();
     ll ans = 0;
-    for(auto [x, op, idx] : entries){        
+    for(auto [x, op, idx] : entries){                
         if(removed[idx]) continue;
         if(op == 0) curr.push_back(idx);
         else{   
             if(sz(curr) == 0) continue;
             int fidx=  curr.front();
             int lidx = curr.back();            
-
+            removed[fidx] = 1;
+            
             int bst = sparse::query(a[lidx], b[fidx]);            
             for(int i = 1; i < sz(curr); i++){
                 int pidx = curr[i];
-                bst = min(bst, sparse::query(a[pidx-1], a[pidx]-1) + sparse::query(a[lidx], b[pidx]));                
-            }
+                removed[pidx] = 1;                
+                bst = min(bst, sparse::query(a[curr[i-1]], a[pidx]-1) + sparse::query(a[lidx], b[pidx]));                
+            }            
             ans += bst;
             curr.clear();
         }
