437559fe24a590d0768786c14a17f6f4f15d43ee
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2025-10-24 08:31:24 -0300

    add

--- competition/icpc/europe/benelux/2024/m.cpp
+++ competition/icpc/europe/benelux/2024/m.cpp
@@ -22,23 +22,8 @@ void dbg_out(string s, H h, T... t){
 #else
 #define dbg(...) 42
 #endif
-
-const int MAX2 = 20;
 const int MAX = 2e5+10;
-
-namespace sparse {
-	int m[MAX2][MAX], n;
-	void build(int n2, int* v) {
-		n = n2;
-		for (int i = 0; i < n; i++) m[0][i] = v[i];
-		for (int j = 1; (1<<j) <= n; j++) for (int i = 0; i+(1<<j) <= n; i++)
-			m[j][i] = min(m[j-1][i], m[j-1][i+(1<<(j-1))]);
-	}
-	int query(int a, int b) {
-		int j = __builtin_clz(1) - __builtin_clz(b-a+1);
-		return min(m[j][a], m[j][b-(1<<j)+1]);
-	}
-}
+int v[MAX];
 
 signed main(){
     darvem;
@@ -46,9 +31,7 @@ signed main(){
     int n, m;
     cin >> n >> m;
 
-    int v[MAX];
-    for(int i = 0; i < n; i++) cin >> v[i];
-    sparse::build(n, v);
+    for(int i = 0; i < n; i++) cin >> v[i+1];    
 
     vector<tuple<int, int, int>> entries;    
     vector<int> a(m);
@@ -72,8 +55,7 @@ signed main(){
                 repeatede[e] = {s, i};
             } else continue;            
         } else repeatede[e] = {s, i};
-
-        s--; e--;        
+            
         entries.push_back({s, 0, i});
         entries.push_back({e, 1, i});
         a[i] = s;
@@ -95,28 +77,30 @@ signed main(){
             }
         }
     }
+    
+    vector<ll> dp(n+2);
+    vector<int> ran(n+2, -1);
 
-    curr.clear();
-    ll ans = 0;
     for(auto [x, op, idx] : entries){                
-        if(removed[idx]) continue;
-        if(op == 0) curr.push_back(idx);
-        else{   
-            if(sz(curr) == 0) continue;
-            int fidx=  curr.front();
-            int lidx = curr.back();            
-            removed[fidx] = 1;
-            
-            int bst = sparse::query(a[lidx], b[fidx]);            
-            for(int i = 1; i < sz(curr); i++){
-                int pidx = curr[i];
-                removed[pidx] = 1;                
-                bst = min(bst, sparse::query(a[curr[i-1]], a[pidx]-1) + sparse::query(a[lidx], b[pidx]));                
-            }            
-            ans += bst;
-            curr.clear();
-        }
-    }
-    cout << ans << endl;
+        if(removed[idx] or op == 0) continue;        
+        ran[x+1] = a[idx];
+    }    
 
+    int cur = -1;
+    vector<pair<ll, int>> hp;
+    for(int i = 1; i <= n+1; i++){
+        cur = max(ran[i], cur);        
+        if(cur == -1)dp[i] = v[i];
+        else{
+            while(hp.front().second < cur){                
+                pop_heap(hp.begin(), hp.end());
+                hp.pop_back();
+            }              
+            dp[i] = -hp.front().first + v[i];
+        }        
+        hp.push_back({-dp[i], i});
+        push_heap(hp.begin(), hp.end());
+    }
+
+    cout << dp[n+1] << endl;
 }
