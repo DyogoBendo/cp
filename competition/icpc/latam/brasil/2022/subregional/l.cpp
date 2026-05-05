d692c60df8dc7c3918d9fe8c9d0634cd9f0fba66
dyogo.bendo <dyogoromagnabendo@gmail.com>
2026-05-05 13:20:08 -0300

    add

---
+++ competition/icpc/latam/brasil/2022/subregional/l.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define darvem ios_base::sync_with_stdio(0); cin.tie(0)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(a) (int) (a).size()
+#define ll long long
+#define ld long double
+
+void dbg_out(string s) { cerr << endl; }
+template<typename H, typename... T>
+void dbg_out(string s, H h, T... t){
+    do{ cerr << s[0]; s = s.substr(1);
+    } while (sz(s) && s[0] != ',');
+    cerr << " = " << h;
+    dbg_out(s, t...);
+}
+
+#ifdef DEBUG
+#define dbg(...) dbg_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+signed main(){
+    darvem;
+
+    int n;
+    cin >> n;
+
+    vector<int> color(n);
+    for(int i = 0; i < n; i++) cin >> color[i];
+
+    vector<vector<int>> tree(n);
+    for(int i = 0; i < n-1; i++){
+        int u, v;
+        cin >> u >> v;
+        u--;v --;
+    }
+
+    function<void(int, int)> dfs = [&] (int curr, int p){
+        for(auto e : tree[curr]) if(e != p) {
+            dfs(e, curr);
+        }
+        
+    };
+}
