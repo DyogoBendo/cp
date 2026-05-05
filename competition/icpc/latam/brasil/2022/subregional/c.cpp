7e40ef1f8f3db302f60e59a2a4174971296a65a9
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2026-05-04 22:03:00 -0300

    add

--- competition/icpc/latam/brasil/2022/subregional/c.cpp
+++ competition/icpc/latam/brasil/2022/subregional/c.cpp
@@ -26,6 +26,7 @@ void dbg_out(string s, H h, T... t){
 const int MAX = 1e3 + 10;
 
 int grid[MAX][MAX];
+int marked[MAX][MAX];
 
 signed main(){
     darvem;
@@ -36,15 +37,22 @@ signed main(){
 	vector<pair<int, int>> v(n);
 	for(int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
 
-	for(int i = 0; i < n-1; i++) grid[v[i].second][v[i].first] = 1;
+	for(int i = 0; i < n-1; i++) marked[v[i].second][v[i].first] = grid[v[i].second][v[i].first] = 1;
 
 	int ans = 0;
 	for(int x = 0; x < MAX-1; x++){
-		for(int y = 0; y < MAX-1; y++){
+		for(int y = 0; y < MAX-1; y++){						
+			if(grid[y][x] and marked[y][x]){
+				grid[y+1][x] ^= 1;
+			}
 			ans += grid[y][x];
-			grid[y+1][x] ^= grid[y][x];
 			grid[y][x+1] ^= grid[y][x];
 		}
+	}
+
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++) cout << grid[i][j] << " ";
+		cout << endl;
 	}
 
 	cout << ans << endl;
