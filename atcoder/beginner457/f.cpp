16e178c7f09cf45c5904f3e1247b3024dcdf24ae
dyogo.bendo <dyogoromagnabendo@gmail.com>
2026-05-11 17:07:14 -0300

    add

--- atcoder/beginner457/f.cpp
+++ atcoder/beginner457/f.cpp
@@ -23,15 +23,161 @@ void dbg_out(string s, H h, T... t){
 #define dbg(...) 42
 #endif
 
-void solve(){
+template<int p> struct mod_int {
+	ll expo(ll b, ll e) {
+		ll ret = 1;
+		while (e) {
+			if (e % 2) ret = ret * b % p;
+			e /= 2, b = b * b % p;
+		}
+		return ret;
+	}
+	ll inv(ll b) { return expo(b, p-2); }
 
+	using m = mod_int;
+	int v;
+	mod_int() : v(0) {}
+	mod_int(ll v_) {
+		if (v_ >= p or v_ <= -p) v_ %= p;
+		if (v_ < 0) v_ += p;
+		v = v_;
+	}
+	m& operator +=(const m& a) {
+		v += a.v;
+		if (v >= p) v -= p;
+		return *this;
+	}
+	m& operator -=(const m& a) {
+		v -= a.v;
+		if (v < 0) v += p;
+		return *this;
+	}
+	m& operator *=(const m& a) {
+		v = 1LL * v * (a.v) % p;
+		return *this;
+	}
+	m& operator /=(const m& a) {
+		v = v * inv(a.v) % p;
+		return *this;
+	}
+	m operator -(){ return m(-v); }
+	m& operator ^=(ll e) {
+		if (e < 0) {
+			v = inv(v);
+			e = -e;
+		}
+		v = expo(v, e);
+		// possivel otimizacao:
+		// cuidado com 0^0
+		// v = expo(v, e%(p-1)); 
+		return *this;
+	}
+	bool operator ==(const m& a) { return v == a.v; }
+	bool operator !=(const m& a) { return v != a.v; }
+
+	friend istream& operator >>(istream& in, m& a) {
+		ll val; in >> val;
+		a = m(val);
+		return in;
+	}
+	friend ostream& operator <<(ostream& out, m a) {
+		return out << a.v;
+	}
+	friend m operator +(m a, m b) { return a += b; }
+	friend m operator -(m a, m b) { return a -= b; }
+	friend m operator *(m a, m b) { return a *= b; }
+	friend m operator /(m a, m b) { return a /= b; }
+	friend m operator ^(m a, ll e) { return a ^= e; }
+};
+
+typedef mod_int<(int)998244353> mint;
+
+const int MAX = 2e5 + 100;
+namespace seg {
+	mint seg[4*MAX], lazy[4*MAX];
+	int n;
+    mint* v;
+
+	mint build(int p=1, int l=0, int r=n-1) {
+		lazy[p] = 1;
+		if (l == r) return seg[p] = v[l];
+		int m = (l+r)/2;
+		return seg[p] = build(2*p, l, m) + build(2*p+1, m+1, r);
+	}
+	void build(int n2, mint* v2) {
+		n = n2, v = v2;
+		build();
+	}
+	void prop(int p, int l, int r) {
+		seg[p] *= lazy[p];
+		if (l != r) lazy[2*p] *= lazy[p], lazy[2*p+1] *= lazy[p];
+		lazy[p] = 1;
+	}
+	mint query(int a, int b, int p=1, int l=0, int r=n-1) {
+		prop(p, l, r);
+		if (a <= l and r <= b) return seg[p];
+		if (b < l or r < a) return 0;
+		int m = (l+r)/2;
+		return query(a, b, 2*p, l, m) + query(a, b, 2*p+1, m+1, r);
+	}
+	mint update(int a, int b, int x, int p=1, int l=0, int r=n-1) {
+		prop(p, l, r);
+		if (a <= l and r <= b) {
+            seg[p] += x;
+			return seg[p];
+		}
+		if (b < l or r < a) return seg[p];
+		int m = (l+r)/2;
+		return seg[p] = update(a, b, x, 2*p, l, m) +
+			update(a, b, x, 2*p+1, m+1, r);
+	}
+
+    mint update_mul(int a, int b, int x, int p=1, int l=0, int r=n-1) {
+		prop(p, l, r);
+		if (a <= l and r <= b) {
+			lazy[p] *= x;
+			prop(p, l, r);
+			return seg[p];
+		}
+		if (b < l or r < a) return seg[p];
+		int m = (l+r)/2;
+		return seg[p] = update_mul(a, b, x, 2*p, l, m) +
+			update_mul(a, b, x, 2*p+1, m+1, r);
+	}
+};
+
+
+mint vx[MAX];
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for(int i = 0; i < n-1; i++) cin >> v[i];
+    
+    seg::build(n, vx);
+    seg::update(n-1, n-1, 1);
+    seg::update(n-2, n-2, 1);
+
+    for(int i = n-3; i>= 0; i--){
+        mint x = seg::query(i+v[i], i+v[i]);
+        if(v[i] == v[i+1]){
+            seg::update_mul(i+1, n-1, n-i-2);
+        } else{
+            seg::update_mul(i+1, n-1, 0);
+        }
+        seg::update(i, i, x.v);
+        seg::update(i+v[i], i+v[i], x.v);
+    }
+
+    cout << seg::query(0, n-1) << endl;
 }
 
 
 signed main(){
     darvem;
     int t = 1;
-    cin >> t;
 
     while(t--) solve();
 }
