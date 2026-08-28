04c6a63940c9e89889b8bff890acab17aa341dd3
dyogo.bendo <dyogoromagnabendo@gmail.com>
2026-08-28 10:38:31 -0300

    add

---
+++ utils/stress/gen.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define darvem ios_base::sync_with_stdio(0); cin.tie(0)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(a) (int) (a).size()
+#define ll long long
+#define ld long double
+
+void dbg_out(string s) { cerr << endl; }
+template<typename H, typename... T>
+void dbg_out(string s, H h, T... t){
+    do{ cerr << s[0]; s = s.substr(1);
+    } while (sz(s) && s[0] != ',');
+    cerr << " = " << h;
+    dbg_out(s, t...);
+}
+
+#ifdef DEBUG
+#define dbg(...) dbg_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+
+int uniform(int l, int r, mt19937 &rng){
+	uniform_int_distribution<int> uid(l, r);
+	return uid(rng);
+}
+
+signed main(int argc, char *argv[]){
+    darvem;
+    
+    int x = atoi(argv[1]);
+
+    mt19937 rng(x);
+
+    cout << uniform(1, 2, rng) << endl;
+}
