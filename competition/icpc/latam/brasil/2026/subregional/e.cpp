a59ed84fc51fe0a07b597f6ee7c03d7d2cad3e1f
Dyogo Romagna Bendo <dyogoromagnabendo@gmail.com>
2026-09-02 14:09:03 -0300

    add

---
+++ competition/icpc/latam/brasil/2026/subregional/e.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define darvem ios_base::sync_with_stdio(0); cin.tie(0)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(a) (int) (a).size()
+#define ll long long
+#define ld long double
+
+void dbg_out(string s) { cerr << endl; }
+template<typename H, typename... T>
+void dbg_out(string s, H h, T... t){
+    do{ cerr << s[0]; s = s.substr(1);
+    } while (sz(s) && s[0] != ',');
+    cerr << " = " << h;
+    dbg_out(s, t...);
+}
+
+#ifdef DEBUG
+#define dbg(...) dbg_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+const int INF = 1e9;
+
+struct dinitz {
+	const bool scaling = false; // com scaling -> O(nm log(MAXCAP)),
+	int lim;                    // com constante alta
+	struct edge {
+		int to, cap, rev, flow;
+		bool res;
+		edge(int to_, int cap_, int rev_, bool res_)
+			: to(to_), cap(cap_), rev(rev_), flow(0), res(res_) {}
+	};
+
+	vector<vector<edge>> g;
+	vector<int> lev, beg;
+	ll F;
+	dinitz(int n) : g(n), F(0) {}
+
+	void add(int a, int b, int c) {
+		g[a].emplace_back(b, c, g[b].size(), false);
+		g[b].emplace_back(a, 0, g[a].size()-1, true);
+	}
+	bool bfs(int s, int t) {
+		lev = vector<int>(g.size(), -1); lev[s] = 0;
+		beg = vector<int>(g.size(), 0);
+		queue<int> q; q.push(s);
+		while (q.size()) {
+			int u = q.front(); q.pop();
+			for (auto& i : g[u]) {
+				if (lev[i.to] != -1 or (i.flow == i.cap)) continue;
+				if (scaling and i.cap - i.flow < lim) continue;
+				lev[i.to] = lev[u] + 1;
+				q.push(i.to);
+			}
+		}
+		return lev[t] != -1;
+	}
+	int dfs(int v, int t, int f = INF) {
+		if (!f or v == t) return f;
+		for (int& i = beg[v]; i < g[v].size(); i++) {
+			auto& e = g[v][i];
+			if (lev[e.to] != lev[v] + 1) continue;
+			int foi = dfs(e.to, t, min(f, e.cap - e.flow));
+			if (!foi) continue;
+			e.flow += foi, g[e.to][e.rev].flow -= foi;
+			return foi;
+		}
+		return 0;
+	}
+	ll max_flow(int s, int t) {
+		for (lim = scaling ? (1<<30) : 1; lim; lim /= 2)
+			while (bfs(s, t)) while (int ff = dfs(s, t)) F += ff;
+		return F;
+	}
+};
+
+signed main(){
+    darvem;
+    int n, m1, m2;
+    cin >> n >> m1 >> m2;    
+
+    set<pair<int, int>> s1, s2;
+
+    for(int i = 0; i < m1; i++){
+        int a, b;
+        cin >> a >> b;
+        if(a > b) swap(a, b);
+        s1.insert({a, b});
+    }
+    
+    for(int i = 0; i < m2; i++){
+        int a, b;
+        cin >> a >> b;
+        if(a > b) swap(a, b);
+        if(s1.count({a, b})){
+            s1.erase({a, b});
+        } else{
+            s2.insert({a, b});
+        }
+    }
+    
+    if(m1 != m2){
+        cout << -1 << endl;
+        return 0;
+    }
+
+    int m = sz(s1);
+
+    dbg(m);
+
+    dinitz d(1 + 2* m + 1);
+
+    int c1=1;
+    for(auto [a, b] : s1){
+        d.add(0, c1, 1);
+        int c2 = 1 + m;
+        for(auto [x, y] : s2){
+            if(x == a or x == b or y == a or y == b) d.add(c1, c2, 1);
+            c2++;
+        }
+        c1++;
+    }
+    for(int i = 0; i < m; i++) d.add(m + 1 + i, 2*m + 1, 1);
+    
+    cout << 2*m - d.max_flow(0, 2*m+1) << endl;
+}
---
+++ competition/icpc/latam/brasil/2026/subregional/i.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define darvem ios_base::sync_with_stdio(0); cin.tie(0)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(a) (int) (a).size()
+#define ll long long
+#define ld long double
+
+void dbg_out(string s) { cerr << endl; }
+template<typename H, typename... T>
+void dbg_out(string s, H h, T... t){
+    do{ cerr << s[0]; s = s.substr(1);
+    } while (sz(s) && s[0] != ',');
+    cerr << " = " << h;
+    dbg_out(s, t...);
+}
+
+#ifdef DEBUG
+#define dbg(...) dbg_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+#define int ll
+
+signed main(){
+    darvem;
+
+    int f, n;
+    cin >> f >> n;
+    
+    vector<int> all_values, friend_group(n+1), idx_to_k(n+1);
+    vector<pair<int, int>> entries;    
+    for(int i = 1; i <= n; i++){
+        int k, p;
+        cin >> k >> p;
+
+        idx_to_k[i] = k;
+        all_values.push_back(k);        
+        entries.push_back({k, p});
+    }
+    sort(all_values.begin(), all_values.end());
+
+    map<int, int> mp;
+    int cnt=1;
+    for(int k : all_values) mp[k] = cnt++;
+    for(auto [k, p] : entries) friend_group[mp[k]] = p;    
+
+    vector<int>  status(n+1), depends(n+1, -1), arrival(n+1), duration(n+1);
+    vector<int> dependents(n+1);
+
+    vector<int> all_times;
+
+    for(int i = 1; i <= n; i++){
+        char T;
+        cin >> T;
+
+        int k = mp[idx_to_k[i]];
+        if(T == 'A'){
+            int a, t;
+            cin >> a >> t;
+            status[k] = 1;
+            arrival[k] = a;
+            duration[k] = t;            
+        } else if(T == 'T'){
+            int x;
+            cin >> x;
+            x = mp[x];            
+            depends[k] = x;
+            dependents[x]++;
+        }
+    }    
+
+    queue<int> q;
+    for(int i = 1; i<= n; i++){
+        if(dependents[i] == 0) q.push(i);
+    }
+
+    while(!q.empty()){
+        int curr = q.front(); q.pop();
+        int dep = depends[curr];
+        if(dep != -1){
+            friend_group[dep] += friend_group[curr];
+            dependents[dep] --;            
+            if(dependents[dep] == 0) q.push(dep);
+        }
+    }    
+
+    vector<tuple<int, int, int>> events;
+    for(int i = 1; i <= n; i++){
+        if(status[i] == 1){
+            events.push_back({arrival[i], 1, friend_group[i]});
+            events.push_back({arrival[i] + duration[i], 0, friend_group[i]});
+        }
+    }
+
+    sort(events.begin(), events.end());
+
+    ll ans = 0;   
+    ll curr = 0; 
+    for(auto [t, o, s] : events){
+        if(o == 0) curr -= s;
+        else curr += s;
+        ans = max(ans, curr);
+    }
+
+    cout << ans << endl;
+}
